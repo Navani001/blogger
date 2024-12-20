32a196298c760341223a9e36c756cbc11a6a510a
navani_hk <navaneetha2006krishnan@gmail.com>
2024-12-20 12:17:49 +0530

    fixing bugs in viewing

--- src/app/blogs/[url]/page.tsx
+++ src/app/blogs/[url]/page.tsx
@@ -3,7 +3,7 @@ import get_article from "@/lib/blogfetch";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-export default function Home({ params }: { params: { url: string } }) {
+export default function Home({ params }: { params: any }) {
     const { url } = params;
     console.log(url)
     const[content,setcontent]=useState("")
