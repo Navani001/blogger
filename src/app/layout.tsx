bea3b0d9383ea33f613f801a7e2ed40cebf43368
navani_hk <navaneetha2006krishnan@gmail.com>
2024-12-21 08:04:55 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <meta name="google-site-verification" content="fxv7potWOmdmlPWIRXu2haQGeoWZqrqLYZ0j1Rx9TNI" />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8518220968118950"
      crossOrigin="anonymous"></script>
 
@@ -33,6 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        
       </body>
     </html>
   );
