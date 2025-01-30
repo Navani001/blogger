4c7be9feee14a49bae2549ed6c030e15293b2e03
navani_hk <navaneetha2006krishnan@gmail.com>
2025-01-30 20:35:42 +0530

    ..

--- src/ui/components/model/share/sharebody.tsx
+++ src/ui/components/model/share/sharebody.tsx
@@ -139,7 +139,7 @@ export function ShareBody({
             </Button>
 
             <TwitterShareButton url={shareUrl} title={"share this page"} className="w-32">
-              <div className="gap-0 flex p-[14px] bg-[#1DA1F2]  rounded-md items-center text-white">
+              <div className="flex p-[14px] gap-2 bg-[#1DA1F2]  rounded-md items-center text-white">
                 <ImTwitter size={20} className="m"/>
                 Twitter</div>
             </TwitterShareButton>
