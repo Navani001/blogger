04ea313463b794c1a26caf133398edf1b0bf2dc9
navani_hk <navaneetha2006krishnan@gmail.com>
2025-06-15 11:20:13 +0530

    ad sence

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,14 +36,10 @@ export default function RootLayout({
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
 <link rel="manifest" href="/site.webmanifest"></link>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8518220968118950"
-          crossOrigin="anonymous"
+        
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8518220968118950"
+          crossOrigin="anonymous"></script>
 
-        ></script>
- 
-       
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
