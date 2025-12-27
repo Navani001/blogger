d7ed36b13bdcacf9edc864d46ae979be9ad49674
navani_hk <149717513+Navani001@users.noreply.github.com>
2025-12-27 13:41:57 +0530

    Update model version from gemini-2.0 to gemini-2.5

--- src/app/api/ai/custom/route.ts
+++ src/app/api/ai/custom/route.ts
@@ -9,7 +9,7 @@ console.log("request called")
 console.log(messages)
     const result = await streamText({
         system:`you are a assistence for user your job it is full fill users request and also you want to produce the content in html tag without html inclosed start you work no eed for formalities no need for html,head tag now do`,   
-        model: google("gemini-2.0-flash"),
+        model: google("gemini-2.5-flash"),
         prompt:messages[messages.length - 1].content+""
       
     })
